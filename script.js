5e6fb85357c34d00ad039fd43e36f11daa885483
Vishal Vashishth <vishal112vp@gmail.com>
2026-04-16 19:50:24 +0530

    building blocks

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const blockWidth = 30;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
+
+// for (let i = 0; i < rows * cols; i++) {
+//     const block = document.createElement('div');
+//     block.classList.add('block');
+//     board.appendChild(block);
+// }
+
+for(let row = 0; row < rows; row++){
+    for(let col = 0; col < cols; col++){ 
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+    }
+}
